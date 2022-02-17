c13c2a11be15c70dddf6cdbe17a3f0a69806ec0d
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-17 23:08:58 +0100

    Feat: Setup error handling in express
    - Setup error handling in goalsController.js using uiltin express errorhandler
    - Override the builtin error handler using errorMiddleware.js in middleware directory
    - Import the errorHandler middleware in server.js

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -11,6 +11,11 @@ const getGoals = (req, res) => {
 // @access Private
 
 const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Set goal" });
 };
 
--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 8000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.use(errorHandler);
 
 const listener = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
