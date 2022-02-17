c765677f2d308cfd67a57fbdc4e726659e2526b4
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-17 20:53:52 +0100

    Feat: Created controller functions for the routes
    - Created goalController.js in controller directory
    - Created CRUD controller functions to be used in the routes
    - Cleaned up the routes.js by chaining methods with the same routes

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -1,21 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-  // make a request to home route "/" because the route has been specified in server.js app.use
-  res.status(200).json({ message: "Get goals" }); // try to set the response status
-});
+router.route("/").get(getGoals).post(setGoal);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goal" });
-});
-
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
