e58cf96988aa1e8c661a292c6cc4b3090a7aa459
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-02-17 23:58:52 +0100

    Feat: Created mongoDB connection in config folder
    - Created a new MongoDB connection in db.js in config directory
    - Imported and highlighted connection string in cli with colors package in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
-
+const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
+
+connectDB();
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
