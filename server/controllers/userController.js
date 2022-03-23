6a67214f7507c68666d9ec988d075ba206ea3211
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-03-23 12:08:23 +0100

    refactor: removed fing user step in controllers
    - removed the find user step in goal and user controller since the req.user is already set in the authMiddleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -73,14 +73,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route POST /api/user/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-  // we now have access req.user in any route we use the authMiddleware because we set the req.user
-  const { _id, name, email } = await User.findById(req.user.id);
-
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
+  res.status(200).json(req.user);
 });
 
 // Generate JWT
