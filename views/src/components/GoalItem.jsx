9203c045f20c794190aa5568feaebfdde5a47775
Tony Edgal <78075405+tonyedgal@users.noreply.github.com>
2022-03-24 13:38:36 +0100

    feat: completed tutorial
    - Updated dashboard component
    - Protected route by redirecting non logged-in user to login
    - Set spinner component on isLoading state
    - Added goalform component to create goalform
    - Added goalitem component to display goals
    - Created goalSlice.js and goalService.js in features/goals to manage goals state
    - Imported the goalReducer to store.js

---
+++ views/src/components/GoalItem.jsx
@@ -0,0 +1,17 @@
+import { MdDeleteForever } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deleteGoal } from "../features/goals/goalSlice";
+
+export default function GoalItem({ goal }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="goal">
+      <div>{new Date(goal.createdAt).toLocaleString("en-US")}</div>
+      <h2>{goal.text}</h2>
+      <button onClick={() => dispatch(deleteGoal(goal._id))} className="close">
+        <MdDeleteForever />
+      </button>
+    </div>
+  );
+}
